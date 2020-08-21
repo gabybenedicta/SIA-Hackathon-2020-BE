dd3952b5eb7ab23b4328eec0c2d8ca9564dd217b
gabybenedicta <gaby.benedicta@yahoo.com>
2020-08-22 02:59:40 +0800

    push to master

--- app/models.py
+++ app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+	name = models.CharField(max_length=30)
+	kf_member= models.IntegerFeld()
+	is_lounge = models.BooleanField(default=False)
+	is_shower = models.BooleanField(default=False)
+
+class Lounge(models.Model):
+	name = models.CharField(max_length=30)
+
+class ShowerStall(model.Models):
+	lounge = models.ForeignKey(Lounge, on_delete=models.CASCADE)
+	is_vacant = models.BooleanField(default=True)
