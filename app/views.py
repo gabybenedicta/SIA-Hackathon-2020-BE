642c6077dd7f379f88d71605451c0fce128c7fd1
gabybenedicta <gaby.benedicta@yahoo.com>
2020-08-22 04:08:57 +0800

    stringify

--- app/views.py
+++ app/views.py
@@ -6,10 +6,10 @@ from rest_framework.decorators import api_view
 @api_view(['POST'])
 def process_barcode(request, pk):
     if request.method == 'POST':
-        if request.data["barcodeValue"] == 123457629:
+        if request.data["barcodeValue"] == "123457629":
             returned = "Showers"
             return Response(returned, status = status.HTTP_200_OK)
-        elif request.data["barcodeValue"] == 9189283746:
+        elif request.data["barcodeValue"] == "9189283746":
             returned = "Services"
             return Response(returned, status = status.HTTP_200_OK)
         return Response("Not Found", status= status.HTTP_404_NOT_FOUND)
