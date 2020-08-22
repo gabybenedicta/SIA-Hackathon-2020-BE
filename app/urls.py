13c4fc61432e47c76048d3026138f1fbc34c25d4
Maggie Koesno <maggie.koesno@gmail.com>
2020-08-22 13:45:02 +0800

    Def API points

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('process_barcode/<int:pk>', views.process_barcode),
+    path('join_shower_queue/<int:pk>/lounge/<int:lounge>', views.join_shower_queue),
 ]
--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 
+from models import ShowerStall
 
 @api_view(['POST'])
 def process_barcode(request, pk):
@@ -13,3 +14,16 @@ def process_barcode(request, pk):
             returned = "Services"
             return Response(returned, status = status.HTTP_200_OK)
         return Response("Not Found", status= status.HTTP_404_NOT_FOUND)
+
+@api_view(['GET'])
+def get_shower_queue()
+
+@api_view(['POST'])
+def join_shower_queue(request, pk, lounge):
+    if request.method == 'POST':
+        # check if there is empty stall, if yes right away allow check in
+        vacant_stalls = list(ShowerStall.objects.filter(is_vacant = True))
+        if len(vacant_stalls) > 0:
+            
+
+        # else put user id in queue (based on loungeID 
