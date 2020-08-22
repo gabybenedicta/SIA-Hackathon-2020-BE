b19a19363d796dfc5d16d1815cb8189ea6c3328a
gabybenedicta <gaby.benedicta@yahoo.com>
2020-08-22 12:50:42 +0800

    user

--- app/views.py
+++ app/views.py
@@ -2,10 +2,21 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 
+from .models import User
+
 
 @api_view(['POST'])
 def process_barcode(request, pk):
     if request.method == 'POST':
+        try:
+            user = User.objects.get(uid=pk)
+        except User.DoesNotExist:
+            message = "User does not exist"
+            return Response(message, status=status.HTTP_404_NOT_FOUND)
+        
+        print("USER")
+        print(user)
+
         if request.data["barcodeValue"] == "123457629":
             returned = "Showers"
             return Response(returned, status = status.HTTP_200_OK)
