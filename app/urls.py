d0a417f69c36574ed4bdebb4dc98fe9958f989e9
Maggie Koesno <maggie.koesno@gmail.com>
2020-08-22 15:33:42 +0800

    Add endpoints

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('process_barcode/<int:pk>', views.process_barcode),
-    path('join_shower_queue/<int:pk>/lounge/<int:lounge>', views.join_shower_queue),
+    path('join_shower_queue/<int:pk>/lounge/<int:lounge_id>', views.join_shower_queue),
+    path('check_out_lounge/<int:pk>', views.check_out_lounge),
+    path('check_out_shower/<int:pk>/lounge/<int:lounge_id>', views.check_out_shower),
 ]
--- app/views.py
+++ app/views.py
@@ -2,8 +2,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 
-from .models import User, ShowerQueue
-
+from .models import User, ShowerQueue, ShowerStall
 
 @api_view(['POST'])
 def process_barcode(request, pk):
@@ -24,15 +23,89 @@ def process_barcode(request, pk):
             return Response(returned, status = status.HTTP_200_OK)
         return Response("Not Found", status= status.HTTP_404_NOT_FOUND)
 
-@api_view(['GET'])
-def get_shower_queue()
 
 @api_view(['POST'])
-def join_shower_queue(request, pk, lounge):
+def join_shower_queue(request, pk, lounge_id):
     if request.method == 'POST':
-        # check if there is empty stall, if yes right away allow check in
-        vacant_stalls = list(ShowerStall.objects.filter(is_vacant = True))
-        if len(vacant_stalls) > 0:
-            
+        try:
+            user = User.objects.get(id=pk)
+        except User.DoesNotExist:
+            message = "User does not exist"
+            return Response(message, status=status.HTTP_404_NOT_FOUND)
+        
+        try:
+            lounge = Lounge.objects.get(id=lounge_id)
+        except Lounge.DoesNotExist:
+            message = "Lounge does not exist"
+            return Response(message, status=status.HTTP_404_NOT_FOUND)
 
-        # else put user id in queue (based on loungeID 
+        response = {"isInShower" : False}
+        response["queueLength"] = len(list(ShowerQueue.objects.all()))
+
+        vacant_stalls = list(ShowerStall.objects.filter(is_vacant = True, lounge = lounge))
+        
+        if len(vacant_stalls) > 0:
+            shower_stall = vacant_stalls[0]
+
+            response["canShower"] = True
+            response["isJoined"] = False
+            response["stallEnter"] = shower_stall.id
+            
+            shower_stall.user_id = user.id 
+            shower_stall.is_vacant = False
+            shower_stall.save()
+
+        else:
+            response["isJoined"] = True
+            response["canShower"] = False
+            response["stallEnter"] = None
+            queue = ShowerQueue(user=user)
+            
+        return Response(response, status = status.HTTP_200_OK)
+
+@api_view(['POST'])
+def check_out_shower(request, pk, lounge_id):
+    if request.method == 'POST':
+        try:
+            user = User.objects.get(id=pk)
+        except User.DoesNotExist:
+            message = "User does not exist"
+            return Response(message, status=status.HTTP_404_NOT_FOUND)
+
+        try:
+            lounge = Lounge.objects.get(id=lounge_id)
+        except Lounge.DoesNotExist:
+            message = "Lounge does not exist"
+            return Response(message, status=status.HTTP_404_NOT_FOUND)
+
+        # check out
+        user.is_shower = False
+        user.save()
+
+        # delegate stall
+        stall = list(ShowerStall.objects.filter(user_id = user.id))[0]
+        
+        queues = list(ShowerQueue.objects.all())
+        if queues:
+            stall.user_id = queues[0].user.id
+        else:
+            stall.is_vacant = True
+        
+        stall.save()
+
+        response = {"isJoined" : False, "canShower" : False, "isInShower" : False, "queueLength" : 0, "stallEnter" : None}
+        return Response(response, status = status.HTTP_200_OK)
+
+@api_view(['POST'])
+def check_out_lounge(request, pk):
+    if request.method == 'POST':
+        try:
+            user = User.objects.get(id=pk)
+        except User.DoesNotExist:
+            message = "User does not exist"
+            return Response(message, status=status.HTTP_404_NOT_FOUND)
+        
+        user.is_lounge = False
+        user.save()
+    
+    return Response("Success", status = status.HTTP_200_OK)
